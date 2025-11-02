72f2370d6b88bd25d23a7cdc2c4195cdcc64f26b
Daniel Vollbro <wollbro90@gmail.com>
2025-11-02 20:49:31 +0100

    Init commit

---
+++ apt.go
@@ -0,0 +1,34 @@
+package apt
+
+import (
+	"context"
+	"errors"
+	"fmt"
+)
+
+type Config struct {
+	Packages []string `hcl:"packages,optional"`
+	Update   bool     `hcl:"update,optional"`
+}
+
+type AptPlugin struct {
+	cfg Config
+}
+
+func (a *AptPlugin) Schema() any {
+	return &a.cfg
+}
+
+func (a *AptPlugin) Validate(ctx context.Context) error {
+	if len(a.cfg.Packages) == 0 {
+		return errors.New("at least one package required")
+	}
+	return nil
+}
+
+func (a *AptPlugin) Run(params map[string]any) (string, error) {
+	for _, p := range a.cfg.Packages {
+		fmt.Println("Installing: ", p)
+	}
+	return "APT run completed", nil
+}
